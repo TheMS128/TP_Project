2817c26e299daf0473f005389e89acb8bc4bcd5a
Anomaly User <i@example.com>
2025-12-06 22:39:18 +0300

    Add model, which replace old teacher model

---
+++ CourseProject/Models/SubjectModels/CreateSubjectModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using CourseProject.DataBase.DbModels;
+
+namespace CourseProject.Models.SubjectModels;
+
+public class CreateSubjectModel
+{
+    [Required(ErrorMessage = "Название предмета обязательно")]
+    public string Title { get; set; } = null!;
+    public string? Description { get; set; }
+    public List<string> SelectedTeacherIds { get; set; } = new();
+    public List<User> AvailableTeachers { get; set; } = new();
+}
---
+++ CourseProject/Models/SubjectModels/ManageSubjectsModel.cs
@@ -0,0 +1,10 @@
+﻿using CourseProject.DataBase.DbModels;
+
+namespace CourseProject.Models.SubjectModels;
+
+public class ManageSubjectsModel
+{
+    public List<Subject> Subjects { get; set; } = new();
+    public List<User> AllTeachers { get; set; } = new();
+    public List<Group> AllGroups { get; set; } = new();
+}
