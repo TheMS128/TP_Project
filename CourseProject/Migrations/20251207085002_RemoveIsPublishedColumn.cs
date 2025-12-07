9ab945624e6704eceb059aed33e5a9d1eda27821
Anomaly User <i@example.com>
2025-12-07 17:43:28 +0300

    Smth files (auto generate dotnet)

---
+++ CourseProject/Migrations/20251207085002_RemoveIsPublishedColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CourseProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveIsPublishedColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPublished",
+                table: "Lectures");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPublished",
+                table: "Lectures",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
