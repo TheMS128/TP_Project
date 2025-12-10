2c80c5ef8437d1d25f267670742a6b4bafaeeb4f
Anomaly User <i@example.com>
2025-12-10 17:51:13 +0300

    Fix controller and tests

---
+++ CourseProject.Tests/HomeControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using CourseProject.Controllers;
+using CourseProject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseProject.Tests;
+
+[TestFixture]
+public class HomeControllerTests
+{
+    [Test]
+    public void Index_ReturnsViewResult()
+    {
+        var controller = new HomeController();
+        var result = controller.Index();
+
+        Assert.That(result, Is.InstanceOf<ViewResult>());
+    }
+
+    [Test]
+    public void Error_ReturnsViewResult_With_ErrorViewModel()
+    {
+        var controller = new HomeController();
+        controller.ControllerContext = new ControllerContext
+        {
+            HttpContext = new DefaultHttpContext()
+        };
+
+        var result = controller.Error();
+        Assert.That(result, Is.InstanceOf<ViewResult>());
+
+        var viewResult = result as ViewResult;
+        Assert.That(viewResult?.Model, Is.InstanceOf<ErrorViewModel>());
+    }
+}
